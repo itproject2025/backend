55302da10887b09ea6a05d67a6aefed8cd7be7e5
Seonjeongg <sj030205@naver.com>
2025-06-17 11:49:05 +0900

    초기 세팅

---
+++ src/main/java/com/example/itprojectbackend/ItprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.itprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItprojectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItprojectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itprojectbackend/ItprojectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItprojectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
