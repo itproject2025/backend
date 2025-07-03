51b4f248ae7d1c95bf44b7f1516bc3d307a4dabe
Seonjeongg <sj030205@naver.com>
2025-07-03 17:26:54 +0900

    fix: crewcontroller requestmapping 설정

--- src/main/java/com/itprojectbackend/crew/controller/CrewController.java
+++ src/main/java/com/itprojectbackend/crew/controller/CrewController.java
@@ -9,12 +9,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api/crew")
+@RestController
+@RequestMapping("/api/crew")
 @RequiredArgsConstructor
 @Tag(name = "Crew", description = "승무원 스케줄 관련 API 입니다.")
 public class CrewController {
